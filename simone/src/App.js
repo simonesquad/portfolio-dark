c50f74cdce9857d65f37cf1db309a1f8284d3c3b
Simone Ballard <simone.a.ballard@gmail.com>
2021-03-28 20:50:47 -0600

    App.js updated

--- simone/src/App.js
+++ simone/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+//Components
+import CV from './components/CV';
+import Words from './components/Words';
+import Glitches from './components/Glitches';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <Switch>
+      <Route exact path='/contact' component={Contact} />
+      <Route exact path='/glitches' component={Glitches} />
+      <Route exact path='/words' component={Words} />
+      <Route exact path='/cv' component={CV} />
+      <Route exact path='/' component={Dashboard} />
+    </Switch>
+
+    <Footer />
     </div>
+    </Router>
   );
 }
 
