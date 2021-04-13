7d4a08fa45e3965d1a5ed02ea47b4e1ac7ea20b6
Simone Ballard <simone.a.ballard@gmail.com>
2021-04-12 20:30:19 -0600

    api fetch structure in place

--- simone/src/Words.js
+++ simone/src/Words.js
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import './Words.css'
 //Components
 import Navigation from './Navigation';
 import OnePic from './common/OnePic';
 import Face from './assets/face.jpeg';
 
+const wordsApi = "https://random-words-api.vercel.app/word";
+
 function Words() {
+    const [wordData, setWordData] = useState({});
+
+    useEffect(() => {
+        getWordsWithFetch();
+    }, []);
+
+    const getWordsWithFetch = async () => {
+        const response = await fetch(wordsApi);
+        const jsonData = await response.json();
+        setWordData(jsonData);
+    }
+
     return (
         <div>
             <Navigation 
@@ -23,6 +37,12 @@ function Words() {
             desc='Check Out My Latest on Medium @simone-a-ballard'
             mainimg={Face}
             />
+            <div className="word__wrapper">
+                <button onClick={getWordsWithFetch}>Get Word</button>
+                <h4>{wordData.word}</h4>
+                <h4>{wordData.definition}</h4>
+                <h4>{wordData.pronunciation}</h4>
+            </div>
         </div>
     )
 }
