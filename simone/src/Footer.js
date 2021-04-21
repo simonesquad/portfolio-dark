bb8417c46855b21b6577eaa5c2cb7fe0d774386f
Simone Ballard <simone.a.ballard@gmail.com>
2021-04-21 17:45:11 -0600

    all current styles updated with media breakpoints

--- simone/src/Footer.js
+++ simone/src/Footer.js
@@ -8,45 +8,26 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import './Footer.css' 
 
-const StyledFooter = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 20vh;
-    width: 100vw;
-    color: #eff9f0;
-    background-color: black;
-`
-const StyledCopyright = styled.div`
-    display: flex;
-    margin-left: 1%;
-    margin-top: 7%;
-`
-const StyledIcons = styled.div`
-    display: flex; 
-    flex-direction: row;
-`
-
 const Footer = () => {
     return (
-        <StyledFooter>
-        <StyledCopyright>
+        <div className="footer">
+        <div className="footer__copyright">
         <CopyrightIcon 
-            size="small">
-        </CopyrightIcon>
+            fontSize="small" />
         <h4>2021 | </h4>
         <h4> | Simone Ballard</h4>
-        </StyledCopyright>
-            <div className="footer__buttons">
-            <StyledIcons>
+        </div>
+            <div className="footer__icons">
             <a href="www.github.com/simonesquad" target="_blank">
-            <GitHubIcon size="large" />
+            <GitHubIcon 
+                fontSize="small" />
             </a>
             <a href="www.linkedin.com/in/simoneaballard" target="_blank">
-            <LinkedInIcon size="large" />
+            <LinkedInIcon 
+                fontSize="small" />
             </a>
-            </StyledIcons>
             </div>
-        </StyledFooter>
+        </div>
     )
 }
 
