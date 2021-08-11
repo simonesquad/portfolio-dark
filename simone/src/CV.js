c844841974e4cb11c20d3fd5cb1e80a74c87c5d6
Simone Ballard <simone.a.ballard@gmail.com>
2021-08-11 14:23:42 -0600

    button style updated on CV.js

--- simone/src/CV.js
+++ simone/src/CV.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Button from './common/Button';
 import SCV from './document/simone_ballard.pdf';
 import { Viewer } from '@react-pdf-viewer/core';
 // import Single from "./common/single-page";
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
-// import CVS from './document/SB_xx21.docx';
-// import CV2 from './document/2020_SBallard_CV.pdf';
-// import './CV.css'
+import CVS from './document/simone_ballard.pdf';
+import CV2 from './document/2020_SBallard_CV.pdf';
+import './CV.css'
 
 export default function CV() {
     return (
-        <div className="portfolio">
+        <div>
          <Navigation 
             oneBtnTxt='//'
             oneBtnLink='/'
@@ -23,17 +24,15 @@ export default function CV() {
             fourBtnLink='/contact'
             />
           {/* <Single pdf={SCV} /> */}
+        <div className="cvs">
           <Viewer
             fileUrl={SCV}
-          />;
-        {/* <div className="cv_container">
-        <button className="cv_buttons">
-          <a href = {CVS} rel="noreferrer" target = "_blank">Software</a>
-        </button>
-        <button className="cv_buttons">
-          <a href = {CV2} rel="noreferrer" target = "_blank">Legacy</a>
-        </button>
-      </div> */}
+          />
+        </div>
+        <div className="cv_container">
+        <Button text="Now" link={CVS} rel="noreferrer" target = "_blank" />
+        <Button text="Then" link={CV2} rel="noreferrer" target = "_blank" />
+      </div>
     </div>
         );
     }
