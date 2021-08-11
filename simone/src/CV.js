1dbcd9bf4144f8f00b839dd4994ab52234cd2cb7
Simone Ballard <simone.a.ballard@gmail.com>
2021-08-10 20:15:13 -0600

    build folder copied over

--- simone/src/CV.js
+++ simone/src/CV.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import CVS from './document/SB_xx21.docx';
-import CV2 from './document/2020_SBallard_CV.pdf';
+import { Document } from 'react-pdf/dist/umd/entry.webpack';
 import Navigation from './Navigation';
-import './CV.css'
+// import CVS from './document/SB_xx21.docx';
+// import CV2 from './document/2020_SBallard_CV.pdf';
+// import './CV.css'
 
 export default class CV extends Component {
 
@@ -20,14 +21,17 @@ export default class CV extends Component {
             fourBtnTxt='Contact//'
             fourBtnLink='/contact'
             />
-        <div className="cv_container">
+          <Document
+            file="simone_ballard.pdf"
+          ></Document>
+        {/* <div className="cv_container">
         <button className="cv_buttons">
           <a href = {CVS} rel="noreferrer" target = "_blank">Software</a>
         </button>
         <button className="cv_buttons">
           <a href = {CV2} rel="noreferrer" target = "_blank">Legacy</a>
         </button>
-      </div>
+      </div> */}
     </div>
         );
     }
