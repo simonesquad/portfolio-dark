4739aff5f9ebaf697d58a4cbeebbe5dc3f542181
Simone Ballard <simone.a.ballard@gmail.com>
2021-08-11 13:43:56 -0600

    pdf is rendering

--- simone/src/CV.js
+++ simone/src/CV.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Navigation from './Navigation';
 import SCV from './document/simone_ballard.pdf';
+import { Viewer } from '@react-pdf-viewer/core';
 // import Single from "./common/single-page";
-import { Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
-const defaultLayoutPluginInstance = defaultLayoutPlugin();
 // import CVS from './document/SB_xx21.docx';
 // import CV2 from './document/2020_SBallard_CV.pdf';
 // import './CV.css'
@@ -26,14 +23,9 @@ export default function CV() {
             fourBtnLink='/contact'
             />
           {/* <Single pdf={SCV} /> */}
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
           <Viewer
             fileUrl={SCV}
-            plugins={[
-              defaultLayoutPluginInstance,
-            ]}
-          />
-          </Worker>
+          />;
         {/* <div className="cv_container">
         <button className="cv_buttons">
           <a href = {CVS} rel="noreferrer" target = "_blank">Software</a>
