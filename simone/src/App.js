a0ca8f36dd9869c9132dd50d43e0e5098dc3525a
Simone Ballard <simone.a.ballard@gmail.com>
2022-05-24 17:23:13 -0500

    already so much more responsive

--- simone/src/App.js
+++ simone/src/App.js
@@ -27,7 +27,7 @@ function App() {
     </div>
     </BrowserRouter>
     </Worker>
-    <Footer />
+    {/* <Footer /> */}
     </>
   );
 }
--- simone/src/Dashboard.js
+++ simone/src/Dashboard.js
@@ -2,19 +2,29 @@ import React from 'react';
 
 //Components
 import Navigation from './Navigation';
-import Box from './common/Box';
-import TextBox from './common/TextBox';
+// import BoxDemo from './common/Box';
 import Profile from './assets/la.jpeg';
 import Water from './assets/water.jpeg';
 import Snow from './assets/snow.jpeg';
 import Summer from './assets/summer.jpeg';
 import Tree from './assets/tree.jpeg';
 
+import {
+    Box,
+    Grid,
+    GridItem,
+    Heading,
+    Image,
+    Stack,
+    Text,
+    VStack
+} from '@chakra-ui/react';
+
 
 
 function Dashboard() {
     return (
-        <div>
+        <Stack>
             <Navigation 
             oneBtnTxt='S'
             oneBtnLink='/'
@@ -27,25 +37,44 @@ function Dashboard() {
             fiveBtnTxt='>>>'
             fiveBtnLink='/contact'
             />
-            <div className="box__container">
-            <Box 
-            title='Simone Ballard'
-            desc='Code | Comms | Environment'
-            mainimg={Profile}
-            img2={Water}
-            img3={Tree}
-            img4={Summer}
-            img5={Snow}
-            add_more='' 
-            />
-            <TextBox 
-            title='The journey has been real...'
-            text1='Over these years I have been on a journey into sustainable tech, software development, crypto, and the power of communication to motivate forces for change. This journey has taken me all over the states and world, and I have learned so much from each place, but more importantly I have learned that we are a global movement. With every stage of the journey I continue to leverage my skills and experience towards addressing some of the major societal challenges of today.'
-            text2='I am constantly searching for interesting projects and people who are making waves in their respective fields. Ready to discuss how we can build something fascinating together.'
-            />
-            </div>
+        <VStack
+            w='100%'
+            h='75vh'
+        >
+        <Grid templateColumns='repeat(5, 1fr)' gap={1.5}>
+            <GridItem w='100%' h='10'>
+               <Image src={Profile} alt='me' />
+            </GridItem>
+            <GridItem w='100%' h='10'>
+                <Image src={Water} alt='water' />
+            </GridItem>
+            <GridItem w='100%' h='10'>
+                <Image src={Snow} alt='snow' />
+            </GridItem>
+            <GridItem w='100%' h='10'>
+                <Image src={Summer} alt='summer' />
+            </GridItem>
+            <GridItem w='100%' h='10'>
+                <Image src={Tree} alt='tree' />
+            </GridItem>
+        </Grid>
+        </VStack>
+        <VStack
+            w='100%'
+            h='55vh'
+        >
+            <Box>
+            <Heading>'The journey has been real...'</Heading>
+            </Box>
+            <Box>
+                <Text>Over these years I have been on a journey into sustainable tech, software development, crypto, and the power of communication to motivate forces for change. This journey has taken me all over the states and world, and I have learned so much from each place, but more importantly I have learned that we are a global movement. With every stage of the journey I continue to leverage my skills and experience towards addressing some of the major societal challenges of today.</Text>
+            </Box>
+            <Box>
+                <Text>I am constantly searching for interesting projects and people who are making waves in their respective fields. Ready to discuss how we can build something fascinating together.</Text>
+            </Box>
+        </VStack>
 
-        </div>
+        </Stack>
     )
 }
 
