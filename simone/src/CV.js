f0b4156f66c8cd5050d1bc7f9dbaad2f25a2a1dd
Simone Ballard <simone.a.ballard@gmail.com>
2022-05-25 17:02:15 -0500

    cv updated with new layout

--- simone/src/CV.js
+++ simone/src/CV.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-import Button from './common/Button';
+// import Button from './common/Button';
 import CVS from './document/S_B_.pdf';
 import CV2 from './document/2020_SBallard_CV.pdf';
 import './CV.css'
 
+import {
+  Button,
+  ButtonGroup,
+  Text,
+  Stack,
+  HStack,
+  VStack,
+} from '@chakra-ui/react';
+
 
 export default function CV() {
-  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
@@ -16,19 +24,30 @@ export default function CV() {
   }
 
     return (
-        <div classname="whole_cv_view">
-        <div className="cvs">
+        <Stack
+          align='center'
+        >
+        <VStack
+          h='117vh'
+        >
         <Document file={CVS} onLoadSuccess={onDocumentLoadSuccess}>
         <Page pageNumber={pageNumber} />
         </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-        </div>
-        <div className="cv_container">
-        <Button text="Now>>" link={CVS} rel="noreferrer" target = "_blank" />
-        <Button text="Then>>" link={CV2} rel="noreferrer" target = "_blank" />
-      </div>
-    </div>
+        </VStack>
+        <HStack
+          h='15vh'
+        >
+        <Text>Download:</Text>
+        <ButtonGroup>
+        <a rel="noreferrer" href={CVS} target = "_blank">
+        <Button>Current</Button>
+        </a>
+        <a rel="noreferrer" href={CV2} target = "_blank">
+        <Button>Legacy</Button> 
+        </a>
+        </ButtonGroup>
+        </HStack>
+        
+      </Stack>
         );
     }
