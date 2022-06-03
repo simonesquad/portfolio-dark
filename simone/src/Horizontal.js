5e52e506502475f870469a433c250705c01755a9
Simone Ballard <simone.a.ballard@gmail.com>
2022-06-02 21:02:05 -0500

    test out new conditional

---
+++ simone/src/Horizontal.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import Face from './assets/face.jpeg';
+
+import {
+    Box,
+    Heading,
+    Image,
+    Text,
+    Spacer,
+    HStack,
+} from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '80em',
+    '2xl': '96em',
+  })
+
+function Horizontal() {
+
+    return(
+
+        <HStack
+                w='90%'
+                h={{
+                    sm: '200vh',
+                    lg: '70vh'
+                }}
+            >
+            <Box
+                w='30vw'
+                marginLeft='7vw'
+            >
+            <Image 
+                src={Face} 
+                alt='mural'
+                h={{
+                    sm: '30vh',
+                    lg: '55vh'
+                }}
+                w={{
+                    sm: '25vw',
+                    lg: '20vw'
+                }}
+                borderRadius='50%'
+                />
+            </Box>
+            <Spacer />
+            <Box
+                w='65vw'
+            >
+                <Box
+                    h={{
+                    sm: '20vh',
+                    md: '15vh',
+                    lg: '13vh'
+                }}
+                >
+                <Heading>Language is our key to all the worlds...</Heading>
+                
+                </Box>
+
+                <Box
+                    h={{
+                    sm: '115vh',
+                    md: '75vh',
+                    lg: '57vh'
+                    }}
+                >
+                <Text>I realized early on in my studies and travels that how you communicate an idea is <Text as='b'>half the value of the idea itself.</Text> From working in environmental science during the 2010's, I learned that effecitively communicating the facts of climate change was no small task. <Text as='b'>Facts can easily get diluted or misconstrued in the general narrative behind alterior motives.</Text> None of us realized what a force social media would become for both the good and detriment of society. Coming of age in this era has shaped my approach towards every aspect of my work. Now I can see the good in rapidly communicating and connecting across the globe - especially being remote during a pandemic and needing to still gain information from the international coder community. <Text as='b'>Moving into the next decade I have thoroughly honed my writing style and feel confident in my expert level editing and curation abilities.</Text> While you can still find me on social media and writing articles here and there, I have shifted my focus to my personal projects towards narrative non-fiction stories. I hope you'll join me somewhere on the journey and if you'd like to work together on a project - feel free to reach out.</Text>
+                </Box>
+
+                <Box
+                    h={{
+                        sm: '17vh',
+                        md: '12vh',
+                        lg:'10vh'
+                    }}
+                >
+                <Heading as='h2' size='lg' >Your story is your legacy,
+                </Heading> 
+                <Heading as='h2' size='lg' >make it a good one.</Heading>
+                </Box>
+            </Box>
+            </HStack>
+    )
+}
+
+export default Horizontal
--- simone/src/Words.js
+++ simone/src/Words.js
@@ -1,17 +1,15 @@
 import React from 'react';
-//Component
-import Face from './assets/face.jpeg';
+import Horizontal from './Horizontal';
+import Vertical from './Vertical';
 
 import {
     Box,
     Button,
     Heading,
-    Image,
     Text,
-    Spacer,
     Stack,
-    HStack,
-    VStack
+    VStack,
+    useMediaQuery
 } from '@chakra-ui/react';
 import { createBreakpoints } from '@chakra-ui/theme-tools';
 
@@ -24,6 +22,7 @@ const breakpoints = createBreakpoints({
   })
 
 function Words() {
+    const [isMobile] = useMediaQuery('(max-width: 828px)')
 
     return (
         <Stack>
@@ -41,7 +40,7 @@ function Words() {
             >
             <Box
                 h={{
-                    sm: '23vh',
+                    sm: '25vh',
                     lg: '17vh'
                 }}
             >
@@ -49,10 +48,16 @@ function Words() {
             </Box>
             <Box
                 h={{
-                    sm: '45vh',
+                    sm: '65vh',
                     lg: '30vh'
                 }}
-                marginLeft='9vw'
+                marginLeft={{
+                    sm: '7vw',
+                    lg: '9vw'
+                }}
+                marginTop={{
+                    sm: '5vh'
+                }}
             >
             <Heading as='h1' size='2xl' >If you can make one heap of all your winnings</Heading>
             <Heading as='h2' size='xl' >And risk it on one turn of pitch-and-toss,</Heading>
@@ -61,10 +66,16 @@ function Words() {
             </Box>
             <Box
                 h={{
-                    sm: '45vh',
+                    sm: '65vh',
                     lg: '30vh'
                 }}
-                marginLeft='13vw'
+                marginLeft={{
+                    sm: '7vh',
+                    lg: '13vw'
+                }}
+                marginTop={{
+                    sm: '5vh'
+                }}
             >
             <Heading as='h3' size='lg' >If you can force your heart and nerve and sinew</Heading>
             <Heading as='h3' size='md' >To serve your turn long after they are gone,</Heading>   
@@ -73,8 +84,14 @@ function Words() {
             </Box>
             <Box
                 h={{
-                    sm: '50vh',
+                    sm: '70vh',
                     lg: '30vh'
+                }}
+                marginLeft={{
+                    sm: '7vh',
+                }}
+                marginTop={{
+                    sm: '5vh'
                 }}
             >
             <Heading as='h3' size='lg' >If you can talk with crowds and keep your virtue,</Heading>   
@@ -89,69 +106,12 @@ function Words() {
             </Box>
             </VStack>
 
-            <HStack
-                w='90%'
-                h={{
-                    sm: '200vh',
-                    lg: '70vh'
-                }}
-            >
-            <Box
-                w='30vw'
-                marginLeft='7vw'
-            >
-            <Image 
-                src={Face} 
-                alt='mural'
-                h={{
-                    sm: '30vh',
-                    lg: '55vh'
-                }}
-                w={{
-                    sm: '25vw',
-                    lg: '20vw'
-                }}
-                borderRadius='50%'
-                />
-            </Box>
-            <Spacer />
-            <Box
-                w='65vw'
-            >
-                <Box
-                    h={{
-                    sm: '20vh',
-                    md: '15vh',
-                    lg: '13vh'
-                }}
-                >
-                <Heading>Language is our key to all the worlds...</Heading>
-                
-                </Box>
+            {isMobile ? 
+            <Vertical />
+            :
+            <Horizontal />
+            }
 
-                <Box
-                    h={{
-                    sm: '115vh',
-                    md: '75vh',
-                    lg: '57vh'
-                    }}
-                >
-                <Text>I realized early on in my studies and travels that how you communicate an idea is <Text as='b'>half the value of the idea itself.</Text> From working in environmental science during the 2010's, I learned that effecitively communicating the facts of climate change was no small task. <Text as='b'>Facts can easily get diluted or misconstrued in the general narrative behind alterior motives.</Text> None of us realized what a force social media would become for both the good and detriment of society. Coming of age in this era has shaped my approach towards every aspect of my work. Now I can see the good in rapidly communicating and connecting across the globe - especially being remote during a pandemic and needing to still gain information from the international coder community. <Text as='b'>Moving into the next decade I have thoroughly honed my writing style and feel confident in my expert level editing and curation abilities.</Text> While you can still find me on social media and writing articles here and there, I have shifted my focus to my personal projects towards narrative non-fiction stories. I hope you'll join me somewhere on the journey and if you'd like to work together on a project - feel free to reach out.</Text>
-                </Box>
-
-                <Box
-                    h={{
-                        sm: '17vh',
-                        md: '12vh',
-                        lg:'10vh'
-                    }}
-                >
-                <Heading as='h2' size='lg' >Your story is your legacy,
-                </Heading> 
-                <Heading as='h2' size='lg' >make it a good one.</Heading>
-                </Box>
-            </Box>
-            </HStack>
             <VStack
                 h='40vh'
                 w={{
