100b7ec94752d48b5c77118f0e7f8f90f65ab79d
Simone Ballard <simone.a.ballard@gmail.com>
2023-10-13 19:44:03 -0500

    updated package json

--- simone/src/Footer.js
+++ simone/src/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 
 import {
